60bcde7b21339ac0bb7d7043226817a7f6a8e672
PARTNERS\pchangul <pchangul@cisco.com>
2021-07-30 16:36:02 +0530

    profile change

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,9 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>
           I am Web and NodeJs Application Developer with having 7+ years of
-          experience in building enterpise level application
+          experience in building enterpise level React, Angular & Node application
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
